490ae2421605b95a1437cb408388224043162a0d
paridhijain02 <46786103+paridhijain02@users.noreply.github.com>
2022-03-01 15:19:26 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,77 +1,75 @@
 <?php
-$username=$fullname=$contact="";
-$showalert=false;
-$showerror=false;
-if($_SERVER["REQUEST_METHOD"]=="POST")
-{
-    include 'database.php';
-    $username=$_POST['username'];
-    $fullname=$_POST['fullname'];
-    $contact=$_POST['contact'];
-    $password=$_POST['password'];
-    $cpassword=$_POST['cpassword'];
-    //$exist=false;
-    $status='deactivate';
-    $existsql="Select * FROM `users` WHERE username= '$username'";
-    $result=mysqli_query($conn,$existsql);
-    $numexist=mysqli_num_rows($result);
-    
-    if($numexist==1)
+    $username=$fullname=$contact="";
+    $showalert=false;
+    $showerror=false;
+    if($_SERVER["REQUEST_METHOD"]=="POST")
     {
-        $showerror="This username already exists";
-    }
-    else
-    {
-        if($username=="")
+        include 'database.php';
+        $username=$_POST['username'];
+        $fullname=$_POST['fullname'];
+        $contact=$_POST['contact'];
+        $password=$_POST['password'];
+        $cpassword=$_POST['cpassword'];
+        $status='deactivate';
+        $existsql="Select * FROM `users` WHERE username= '$username'";
+        $result=mysqli_query($conn,$existsql);
+        $numexist=mysqli_num_rows($result);
+        if($numexist==1)
         {
-            $showerror="Username is mandatory";
-        }
-        elseif(($fullname)=="") 
-        {          
-            $showerror = "Name is required";
-        } 
-        elseif(!preg_match("/^[a-zA-Z-' ]*$/",$fullname)) 
-        {
-            $showerror = "Only letters and white space allowed in Fullname";
-        }
-        elseif(strlen($contact)!=0 || strlen($contact)!=0) 
-        {
-            $showerror = "10 numbers for contact";
-        }
-        elseif(!preg_match("/^[0-9-' ]*$/",$contact)) 
-        {
-            $showerror = "Only numbers are allowed in contact";
-        }
-        elseif($password=="")
-        {
-            $showerror="Password is mandatory";
-        }
-        
-        elseif(($password==$cpassword) )
-        {
-            $sql="INSERT INTO `users` (`username`,`fullname`,`contact`, `password`, `regis_date`, `status`) VALUES ('$username','$fullname','$contact', '$password', CURRENT_TIMESTAMP, '$status')";
-            $result=mysqli_query($conn,$sql);
-            $sql1="SELECT sno FROM `users` WHERE `username`='$username'";
-            $result1=mysqli_query($conn,$sql1);
-            $row=mysqli_fetch_assoc($result1);
-            $sno_other_table=$row['sno'];
-            $zero=0;
-            $sql2="INSERT INTO `score_table` (`sno`,`score`) VALUES ('$sno_other_table','$zero')";
-            $result2=mysqli_query($conn,$sql2);
-    
-            if($result)
-            {
-               $showalert= true;
-               $username=$fullname=$contact="";
-               
-            }
+            $showerror="This username already exists";
         }
         else
         {
-            $showerror="Both passwords do not match";
-        }
-    }   
-}
+            if($username=="")
+            {
+                $showerror="Username is mandatory";
+            }
+            elseif(($fullname)=="") 
+            {          
+                $showerror = "Name is required";
+            } 
+            elseif(!preg_match("/^[a-zA-Z-' ]*$/",$fullname)) 
+            {
+                $showerror = "Only letters and white space allowed in Fullname";
+            }
+            elseif(strlen($contact)!=0 || strlen($contact)!=0) 
+            {
+                $showerror = "10 numbers for contact";
+            }
+            elseif(!preg_match("/^[0-9-' ]*$/",$contact)) 
+            {
+                $showerror = "Only numbers are allowed in contact";
+            }
+            elseif($password=="")
+            {
+                $showerror="Password is mandatory";
+            }
+
+            elseif(($password==$cpassword) )
+            {
+                $sql="INSERT INTO `users` (`username`,`fullname`,`contact`, `password`, `regis_date`, `status`) VALUES ('$username','$fullname','$contact', '$password', CURRENT_TIMESTAMP, '$status')";
+                $result=mysqli_query($conn,$sql);
+                $sql1="SELECT sno FROM `users` WHERE `username`='$username'";
+                $result1=mysqli_query($conn,$sql1);
+                $row=mysqli_fetch_assoc($result1);
+                $sno_other_table=$row['sno'];
+                $zero=0;
+                $sql2="INSERT INTO `score_table` (`sno`,`score`) VALUES ('$sno_other_table','$zero')";
+                $result2=mysqli_query($conn,$sql2);
+
+                if($result)
+                {
+                   $showalert= true;
+                   $username=$fullname=$contact="";
+
+                }
+            }
+            else
+            {
+                $showerror="Both passwords do not match";
+            }
+        }   
+    }
 ?>
 
 
@@ -130,7 +128,6 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
                 </form>
                 Already have an account?<button class="btn btn-primary" onclick="window.location.href='login.php'">Login</button> 
             </div>    
-        </div>
-        
+        </div>     
     </body>
 </html>
